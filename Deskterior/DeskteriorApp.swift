cc763ee6f05152ae790185563d529bca28af1e29
132kxx <132kxx@gmail.com>
2023-06-11 14:34:37 +0900

    Initial Commit

---
+++ Deskterior/DeskteriorApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeskteriorApp.swift
+//  Deskterior
+//
+//  Created by kxx on 2023/06/11.
+//
+
+import SwiftUI
+
+@main
+struct DeskteriorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
