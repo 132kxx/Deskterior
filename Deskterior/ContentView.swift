3e4ce8aacaff79d2774c92a61e1c8df1a370ebe3
132kxx <132kxx@gmail.com>
2023-06-13 18:51:23 +0900

    create home view

--- Deskterior/ContentView.swift
+++ Deskterior/ContentView.swift
@@ -10,12 +10,75 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            //navigation title
+            HStack {
+                Text("New")
+                    .font(.largeTitle)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Image(systemName: "person.fill")
+                    .resizable()
+                    .frame(width: 20, height: 20)
+            }
+            .padding(.horizontal)
+
+            ScrollView {
+                VStack {
+
+                    // weekly
+                    VStack {
+                        Text("Weekly")
+                            .font(.title2)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        ScrollView(.horizontal) {
+                            HStack {
+                                ForEach(0...10, id: \.self) {_ in
+                                    Rectangle()
+                                        .frame(width: 150, height: 150)
+                                        .overlay {
+                                            Image(systemName: "heart.fill")
+                                                .foregroundColor(.white)
+                                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                                                .padding(4)
+                                        }
+                                }
+                            }
+                        }
+                    }
+                    // monthly
+                    VStack {
+                        Text("Monthly")
+                            .font(.title2)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        
+                        ScrollView(.horizontal) {
+                            HStack {
+                                ForEach(0...10, id: \.self) {_ in
+                                    Rectangle()
+                                        .frame(width: 150, height: 150)
+                                        .overlay {
+                                            Image(systemName: "heart.fill")
+                                                .foregroundColor(.white)
+                                                .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                                                .padding(4)
+                                            
+                                        }
+                                }
+                            }
+                        }
+                    }
+                    
+                }
+                .padding()
+            }
+            
+            HStack {
+                Image(systemName: "person")
+                Image(systemName: "house")
+                Image(systemName: "bell")
+            }
         }
-        .padding()
     }
 }
 
